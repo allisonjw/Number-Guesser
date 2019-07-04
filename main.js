08ed47f850f332dc88f8c9175ebffe978ad85fb5
Allison Wagner <allisonjw01@gmail.com>
2019-07-03 19:43:54 -0600

    got function working for phase 1

--- main.js
+++ main.js
@@ -1,18 +1,34 @@
+var randomNum = 0;
 var minInput = document.getElementById('min-input-field');
 var maxInput = document.getElementById('max-input-field');
-var updateBtn = document.getElementById('update-button');
+var updateBtn = document.getElementById('update-btn');
 var minNum = document.querySelector('.min-number');
 var maxNum = document.querySelector('.max-number');
 var name1Input = document.getElementById('name-1-input');
 var name2Input = document.getElementById('name-2-input');
 var guess1Input = document.getElementById('guess-1-input');
 var guess2Input = document.getElementById('guess-2-input');
-var submitBtn = document.getElementById('submit-guess-button')
+var submitBtn = document.getElementById('submit-guess-button');
 var resetBtn = document.getElementById('reset-button');
 var clearBtn = document.getElementById('clear-button');
 
+
+
 updateBtn.addEventListener('click', setNumRange);
-updateBtn.addEventListener('click', genRanNumber);
+
+
+function genRanNumber(num1, num2) {
+  num2 = Math.floor(max);
+  num1 = Math.ceil(min);
+  return Math.floor(Math.random() * (num2 - num1 + 1)) + num1;
+};
+
+function setNumRange(e) {
+  e.preventDefault();
+  minNum.innerText = parseInt(minInput.value);
+  maxNum.innerText = parseInt(maxInput.value);
+  genRanNumber();
+}
 
 //FYI: input fields store in strings and pareseInt() will turn into number
 
@@ -21,17 +37,6 @@ updateBtn.addEventListener('click', genRanNumber);
 //update button will generate a random number from min and max range inputs and will
 //be displayed displayed in current range spans in paragraph
 
-function genRanNumber(min, max) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max- min + 1)) + min;
-console.log(getRanNumber(3, 5));
-};
-
-function setNumRange() {
-	minNum.innerText = parseInt(minInput.value);
-	maxNum.innerText = parseInt(maxInput.value);
-}
 
 //name input fields can accept alpha-numeric character and guess input field can accept ONLY numeric value.
 //when player hits submit button the names and current guesses will be displayed in latest 
